729183a17269b5c58559a17634bb2eab5fffeffa
Andrii <andre69tkachenko@ukr.net>
2025-04-10 19:11:08 +0200

    done

--- Lesson 8/Lesson 8.cpp
+++ Lesson 8/Lesson 8.cpp
@@ -20,7 +20,9 @@ void five_numbers()
     cout << "Numbers in reverse order: " << n5 << " " << n4 << " " << n3 << " " << n2 << " " << n1 << endl;
 }
 
+
 // Візьміть задачу з попереднього ДЗ і розширте її: Виведіть на екран всю таблицю множення від 1 до 10
+
 
 void multiplication_table()
 {
@@ -34,56 +36,126 @@ void multiplication_table()
     }
 }
 
+
 /*Що таке "красиве" число? Це число не менше 70, але не більше 500. Це число ділиться на 3, але не ділиться на 8.
  *Або у нього всі цифри однакові*.
 Попросіть користувача ввести "красиве число" і не відчепіться від нього поки він його не введе.
 Таких чисел нам треба від користувача 4. Виведіть середнє з них.*/
 
 
-bool is_same_cifers (int number)
-{
-    int number_length = to_string(abs(number)).length();
-    string number_string = to_string(number);
-    for (int i = 1; i <= number_length; i++)
-    {
-        if (number_string[i] != number_string[i - 1])
-        {
-            return false;
-        }
+bool is_same_digits(int number) {
+    string s = to_string(abs(number));
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i] != s[0]) return false;
     }
     return true;
-    
+}
+
+bool is_beautiful(int n) {
+    return (n >= 70 && n <= 500 && n % 3 == 0 && n % 8 != 0) || is_same_digits(n);
 }
 
 void beautiful_number_finder()
 {
     int beautiful_numbers_amount = 0;
-    while (beautiful_numbers_amount < 5) {
+    int sum = 0;
+
+    while (beautiful_numbers_amount < 4) {
         int number_from_user;
         cout << "Enter a beautiful number: ";
         cin >> number_from_user;
-        if (number_from_user < 70 || number_from_user > 499) {
-            cout << "Not beautiful enough. Try again.\n";
-        }
-        else if (number_from_user % 3 != 0)
-        {
-            cout << "Not beautiful enough. Try again.\n";
-        }
-        else if (number_from_user % 8 == 0)
-        {
-            cout << "Not beautiful enough. Try again.\n";
-        }
-        else if (!is_same_cifers(number_from_user))
-        {
-            cout << "Not beautiful enough. Try again.\n";
-        }
-        else
-        {
+
+        if (is_beautiful(number_from_user)) {
             cout << "NICE ONE! Now one more please :)\n";
+            sum += number_from_user;
             beautiful_numbers_amount++;
+        } else {
+            cout << "Not beautiful enough. Try again.\n";
         }
     }
-    cout <<"Finally you are done!"<< endl;
+
+    double average = static_cast<double>(sum) / 4;
+    cout << "Finally you are done!" << endl;
+    cout << "The average of your beautiful numbers is: " << average << endl;
+}
+
+
+// Намалюйте ялиночку. Напишіть функцію, яка отримує кількість рівнів ялинки й потім відповідно її малює.
+
+
+void draw_tree(int levels) {
+    for (int i = 1; i <= levels; i++) {
+
+        for (int star = 1; star <= i; star++) {
+            cout << "*";
+        }
+
+        for (int plus = 1; plus <= i; plus++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+}
+
+int random_pokemons() {
+    return 3 + rand() % 4;
+}
+
+void competition() {
+    int goal;
+    cout << "Enter initial goal for the competition: ";
+    cin >> goal;
+
+    int bob_wins = 0;
+    int jay_wins = 0;
+
+    while (bob_wins < 3 && jay_wins < 3) {
+        int bob_total = 0;
+        int jay_total = 0;
+        int day = 1;
+
+        cout << "\nCompetition started with goal " << goal << endl;
+
+        while (bob_total < goal && jay_total < goal) {
+            int bob_today = random_pokemons();
+            int jay_today = random_pokemons();
+
+            bob_total += bob_today;
+            jay_total += jay_today;
+
+            cout << "DAY " << day << endl;
+            cout << "Bob caught " << bob_today << " (total: " << bob_total << ")" << endl;
+            cout << "Jay caught " << jay_today << " (total: " << jay_total << ")" << endl;
+            day++;
+        }
+        
+        if (bob_total >= goal && jay_total >= goal) {
+            cout << "It's a tie. They decided to rematch. Competition goal is " << goal << "." << endl;
+        } else if (bob_total >= goal) {
+            bob_wins++;
+            cout << "Bob wins!" << endl;
+            if (bob_wins < 3) {
+                cout << "Jay demands rematch! ";
+                goal += goal / 2;
+                cout << "Competition goal is now " << goal << "." << endl;
+            }
+        } else if (jay_total >= goal) {
+            jay_wins++;
+            cout << "Jay wins!" << endl;
+            if (jay_wins < 3) {
+                cout << "Bob demands rematch! ";
+                goal += goal / 2;
+                cout << "Competition goal is now " << goal << "." << endl;
+            }
+        }
+    }
+
+    cout << "\n=== FINAL RESULT ===" << endl;
+    if (bob_wins == 3) {
+        cout << "Bob wins 3 times! He is the final winner!" << endl;
+    } else {
+        cout << "Jay wins 3 times! He is the final winner!" << endl;
+    }
 }
 
 
@@ -94,6 +166,15 @@ int main()
     multiplication_table();
     
     beautiful_number_finder();
+
+    int levels;
+    cout << "Enter number of levels: ";
+    cin >> levels;
+
+    draw_tree(levels);
+
+    srand(time(0));
+    competition();
     
     return 0;
 }
